06891761589d4dc505f5c98b9bdb26d294d8e912
knoerregaard <knoerregaard@gmail.com>
2020-04-16 13:10:46 +0200

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+# AngQuestServer
+
+## tasks
+
+### Installing dependencies
+- `npm init` new project
+- `tsc init` for initialising the project as a typescript project
+- `npm install express` for new express app, `npm install @types/express` for strong typings
+- `npm install nodemon` for updating node server whenever 
+- `npm install cors` for allowing cross origin request sharing, middleware setup
+
+### Utilizing express
+- `const app = express()` for creating a new express app;
+- `app.get()` get request handlers
+- `app.post()` post request handlers
+
+### Utilizin cors
+- `cors()` as middleware for every request, allowing for cross origin requests
--- server.ts
+++ server.ts
@@ -1,20 +1,25 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+//https://expressjs.com/en/resources/middleware/cors.html
+const cors = require('cors');
 
 let quests = [{
     id : 1, 
-    title : "den første quest"
+    title : "sjette quest"
   },{
     id : 2, 
-    title : "den anden quest"
+    title : "syvende quest"
   },{
     id : 3, 
-    title : "den tredje quest"
+    title : "ottende quest"
   }];
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-app.get('/quests', (req : any, res : any) => {
+// app.use(cors());
+
+app.get('/quests', cors(), (req : any, res : any) => {
     res.send(quests);
-})
+});
